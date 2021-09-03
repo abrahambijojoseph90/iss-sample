3f47fabd4190e7ce8afa205b57ff667b49ab78da
Bijo Joseph Abraham <abrahambijojoseph90@gmail.com>
2021-09-03 20:54:25 +0100

    this is a first commit.

---
+++ isstesting/src/main/java/com/bijoqa/isstesting/App.java
@@ -0,0 +1,13 @@
+package com.bijoqa.isstesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
